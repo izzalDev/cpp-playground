6f6b2a6e26603e56049ad2d9937797fa27e3520f
izzalDev <92254049+izzalDev@users.noreply.github.com>
2024-08-05 22:24:17 +0700

    pointer & reference

---
+++ cpp-dasar/31-reference/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // variabel
+    int a = 5;
+
+    cout << "address dari a " << &a << endl;
+    cout << " nilai dari a "<< a << endl;
+
+    // referance
+    int &b = a;
+
+    cout << " nilai dari b " << b << endl;
+    cout << "address dari b " << &b << endl;
+
+    b = 10;
+    cout << " nilai dari a "<< a << endl;
+    cout << " nilai dari b " << b << endl;
+
+    return 0;
+}
